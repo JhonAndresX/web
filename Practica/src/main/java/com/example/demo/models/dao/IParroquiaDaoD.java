9463e5b3a153e1640e8a93ea97bdc590d327a4ac
BlinkDany <111986563+BlinkDany@users.noreply.github.com>
2023-12-01 10:14:33 -0500

    cambios

--- Practica/src/main/java/com/example/demo/models/dao/IParroquiaDaoD.java
+++ Practica/src/main/java/com/example/demo/models/dao/IParroquiaDaoD.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.demo.models.entity.ParroquiaD;
 
 public interface IParroquiaDaoD extends CrudRepository<ParroquiaD, Long>{
+	
+	
 
 }
