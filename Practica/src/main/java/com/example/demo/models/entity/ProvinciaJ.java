9a43db18024a7b6a46d72663cc92e3475a0400c3
Juanc Idrovo <juancidrovo@gmail.com>
2023-12-01 20:33:31 -0500

    PROVINCIA
    PROVINCIA CLASE, Y DAO

--- Practica/src/main/java/com/example/demo/models/entity/ProvinciaJ.java
+++ Practica/src/main/java/com/example/demo/models/entity/ProvinciaJ.java
@@ -1,5 +1,49 @@
 package com.example.demo.models.entity;
 
-public class ProvinciaJ {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+	@Entity
+	@Table (name = "provincias")
+public class ProvinciaJ implements Serializable{
+	
+	/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+	
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id_provincia;
+
+	private String nombre_provincia;
+
+	/////////// Getter Setter///////////////
+	public Long getId_provincia() {
+		return id_provincia;
+	}
+
+	public void setId_provincia(Long id_provincia) {
+		this.id_provincia = id_provincia;
+	}
+
+	public String getNombre_provincia() {
+		return nombre_provincia;
+	}
+
+	public void setNombre_provincia(String nombre_provincia) {
+		this.nombre_provincia = nombre_provincia;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
