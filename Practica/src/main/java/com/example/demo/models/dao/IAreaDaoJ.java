99e038ac77b0dda91551a2f4575e7d11ce23fbf0
Juanc Idrovo <juancidrovo@gmail.com>
2023-12-04 18:43:15 -0500

    ya salieron los errores faack
    jjj

---
+++ Practica/src/main/java/com/example/demo/models/dao/IAreaDaoJ.java
@@ -0,0 +1,9 @@
+package com.example.demo.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.models.entity.AreaJ;
+
+interface IAreaDaoJ extends CrudRepository<AreaJ, Long>{
+	
+}
---
+++ Practica/src/main/java/com/example/demo/models/services/IAreaServiceJ.java
@@ -0,0 +1,11 @@
+package com.example.demo.models.services;
+
+import java.util.List;
+
+import com.example.demo.models.entity.AreaJ;
+
+public interface IAreaServiceJ {
+	
+	public List<AreaJ> findAll();
+
+}
