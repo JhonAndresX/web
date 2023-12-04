271080054d62f5818e0ad25836bf56e935ea12a8
BlinkDany <111986563+BlinkDany@users.noreply.github.com>
2023-12-04 10:31:28 -0500

    llave foranea en provincia

--- Practica/src/main/java/com/example/demo/models/entity/ProvinciaJ.java
+++ Practica/src/main/java/com/example/demo/models/entity/ProvinciaJ.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -28,6 +29,7 @@ public class ProvinciaJ implements Serializable{
 	private String nombre_provincia;
 
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name="id_provincia")
 	private List<CantonD> listCanton;
 
 	////////// Getters Setters fuck\\\\\\\\\\\
