a94dc22313f7db55e6d64648f3c4272e2c8b11e7
BlinkDany <111986563+BlinkDany@users.noreply.github.com>
2023-12-04 19:26:04 -0500

    error corregido

--- Practica/src/main/java/com/example/demo/models/entity/ParroquiaD.java
+++ Practica/src/main/java/com/example/demo/models/entity/ParroquiaD.java
@@ -21,7 +21,6 @@ public class ParroquiaD implements Serializable{
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)	
 	private Long id;
-	
 	private String nombreParroquia;
 	private Long id_CantonFk;
 	
